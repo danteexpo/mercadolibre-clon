7240847fdf6b5a8cd0ae231e8f22c0350ec97ae6
Dante Expósito <dante0im@gmail.com>
2023-05-08 17:42:57 -0300

    Navbar Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 // img
 import MercadoLibreLogo from '../assets/mercadolibrelogo.svg';
+import DisneyAd from '../assets/disney.svg';
 import Lupa from '../assets/lupa.svg';
 import Mapa from '../assets/mapa.svg';
 import Abajo from '../assets/abajo.svg';
@@ -8,11 +9,27 @@ import Usuario from '../assets/usuario.svg';
 import Campana from '../assets/campana.svg';
 import Carrito from '../assets/carrito.svg';
 
+const generalList = [
+  { name: 'Categorías', icon: null },
+  { name: 'Ofertas', icon: null },
+  { name: 'Historial', icon: null },
+  { name: 'Vender', icon: null },
+  { name: 'Ayuda', icon: null },
+];
+
+const personalList = [
+  { name: null, icon: Usuario },
+  { name: 'Mis compras', icon: null },
+  { name: 'Favoritos', icon: null },
+  { name: null, icon: Campana },
+  { name: null, icon: Carrito },
+];
+
 const Navbar = () => {
   const [input, setInput] = useState('');
 
   return (
-    <nav className="bg-[#FFF159] p-2 pb-3">
+    <nav className="bg-[#FFF159] p-2 pb-3 shadow">
       <div className="max-w-6xl mx-auto flex flex-col gap-4">
         <span className="flex justify-between items-center gap-8 lg:gap-12 xl:gap-16 2xl:gap-20">
           <img src={MercadoLibreLogo} alt="" className="h-8" />
@@ -30,11 +47,7 @@ const Navbar = () => {
               className="absolute right-2 md:right-3 bottom-3 h-4 opacity-60 border-l pl-4 cursor-pointer"
             />
           </span>
-          <img
-            src="https://http2.mlstatic.com/D_NQ_773701-MLA54334638216_032023-OO.webp"
-            alt=""
-            className="h-10"
-          />
+          <img src={DisneyAd} alt="" className="h-10" />
         </span>
         <span className="grid grid-cols-[1fr_3fr_2fr] font-light text-sm">
           <ul>
@@ -50,56 +63,42 @@ const Navbar = () => {
             </li>
           </ul>
           <ul className="flex items-center gap-4">
-            <li className="flex items-center gap-1">
-              <p className="opacity-60 hover:opacity-90 cursor-pointer">
-                Categorías
-              </p>
-              <img src={Abajo} alt="" className="w-2 opacity-25" />
-            </li>
-            <li className="opacity-60 hover:opacity-90 cursor-pointer">
-              Ofertas
-            </li>
-            <li className="opacity-60 hover:opacity-90 cursor-pointer">
-              Historial
-            </li>
-            <li className="opacity-60 hover:opacity-90 cursor-pointer">
-              Vender
-            </li>
-            <li className="opacity-60 hover:opacity-90 cursor-pointer">
-              Ayuda
-            </li>
+            {generalList.map((item, i) => (
+              <li
+                key={i}
+                className={`${item.icon ? 'flex items-center gap-1' : ''}`}
+              >
+                {item.name && (
+                  <p className="opacity-60 hover:opacity-90 cursor-pointer">
+                    {item.name}
+                  </p>
+                )}
+              </li>
+            ))}
           </ul>
           <ul className="place-self-end flex items-center gap-4">
-            <li>
-              <img
-                src={Usuario}
-                alt=""
-                className="h-5 opacity-60 cursor-pointer"
-              />
-            </li>
-            <li className="opacity-60 hover:opacity-90 cursor-pointer">
-              Mis compras
-            </li>
-            <li className="flex items-center gap-1">
-              <p className="opacity-60 hover:opacity-90 cursor-pointer">
-                Favoritos
-              </p>
-              <img src={Abajo} alt="" className="w-2 opacity-25" />
-            </li>
-            <li>
-              <img
-                src={Campana}
-                alt=""
-                className="h-5 opacity-60 cursor-pointer"
-              />
-            </li>
-            <li>
-              <img
-                src={Carrito}
-                alt=""
-                className="h-5 opacity-60 cursor-pointer"
-              />
-            </li>
+            {personalList.map((item, i) => (
+              <li
+                key={i}
+                className={`${item.name ? 'flex items-center gap-1' : ''}`}
+              >
+                {item.name && (
+                  <>
+                    <p className="opacity-60 hover:opacity-90 cursor-pointer">
+                      {item.name}
+                    </p>
+                    <img src={Abajo} alt="" className="w-2 opacity-25" />
+                  </>
+                )}
+                {item.icon && (
+                  <img
+                    src={item.icon}
+                    alt=""
+                    className="h-5 opacity-60 cursor-pointer"
+                  />
+                )}
+              </li>
+            ))}
           </ul>
         </span>
       </div>
